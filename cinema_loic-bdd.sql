f51c3c52b448948a43d4ee03b216f2231b43f8db
BoutinL <boutin.loic@hotmail.com>
2023-07-03 16:44:08 +0200

    export bdd vraie

--- cinema_loic-bdd.sql
+++ cinema_loic-bdd.sql
@@ -14,21 +14,105 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Les données exportées n'étaient pas sélectionnées.
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de la base pour cinema_loic
+CREATE DATABASE IF NOT EXISTS `cinema_loic` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinema_loic`;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de table cinema_loic. acteur
+CREATE TABLE IF NOT EXISTS `acteur` (
+  `id_acteur` int NOT NULL AUTO_INCREMENT,
+  `personne_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_acteur`),
+  KEY `personne_id` (`personne_id`),
+  CONSTRAINT `FK_acteur_personne` FOREIGN KEY (`personne_id`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table cinema_loic.acteur : ~0 rows (environ)
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de table cinema_loic. film
+CREATE TABLE IF NOT EXISTS `film` (
+  `id_film` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dateSortie` datetime DEFAULT NULL,
+  `dureeMinutes` int DEFAULT NULL,
+  `note` int DEFAULT NULL,
+  `affiche` varchar(50) DEFAULT NULL,
+  `realisateur_id` int NOT NULL,
+  PRIMARY KEY (`id_film`),
+  KEY `realisateur_id` (`realisateur_id`),
+  CONSTRAINT `FK_film_realisateur` FOREIGN KEY (`realisateur_id`) REFERENCES `realisateur` (`id_realisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table cinema_loic.film : ~0 rows (environ)
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de table cinema_loic. genre
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id_genre` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table cinema_loic.genre : ~0 rows (environ)
+
+-- Listage de la structure de table cinema_loic. jouer
+CREATE TABLE IF NOT EXISTS `jouer` (
+  `film_id` int DEFAULT NULL,
+  `acteur_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  KEY `film_id` (`film_id`),
+  KEY `acteur_id` (`acteur_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `FK_jouer_acteur` FOREIGN KEY (`acteur_id`) REFERENCES `acteur` (`id_acteur`),
+  CONSTRAINT `FK_jouer_film` FOREIGN KEY (`film_id`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `FK_jouer_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinema_loic.jouer : ~0 rows (environ)
+
+-- Listage de la structure de table cinema_loic. personne
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id_personne` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `sexe` varchar(50) NOT NULL,
+  `dateNaissance` datetime NOT NULL,
+  PRIMARY KEY (`id_personne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinema_loic.personne : ~0 rows (environ)
+
+-- Listage de la structure de table cinema_loic. posseder
+CREATE TABLE IF NOT EXISTS `posseder` (
+  `film_id` int DEFAULT NULL,
+  `genre_id` int DEFAULT NULL,
+  KEY `film_id` (`film_id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `FK_posseder_film` FOREIGN KEY (`film_id`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `FK_posseder_genre` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinema_loic.posseder : ~0 rows (environ)
+
+-- Listage de la structure de table cinema_loic. realisateur
+CREATE TABLE IF NOT EXISTS `realisateur` (
+  `id_realisateur` int NOT NULL AUTO_INCREMENT,
+  `personne_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_realisateur`),
+  KEY `personne_id` (`personne_id`),
+  CONSTRAINT `FK_realisateur_personne` FOREIGN KEY (`personne_id`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinema_loic.realisateur : ~0 rows (environ)
+
+-- Listage de la structure de table cinema_loic. role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table cinema_loic.role : ~0 rows (environ)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
